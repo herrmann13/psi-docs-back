782c3188a8f9c678eaa7203f83958fe5712cd190
Henrique Herrmann <h.herrmann@estudantes.ifg.edu.br>
2026-01-13 17:27:08 -0300

    implementacao basica de server

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,15 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "API online"})
+    res.json({ message: "API Psi-Docs online"});
+})
+
+app.get("/health", (req:Request, res: Response) => {
+    res.json({
+        api:"online",
+        db:{
+            status:"not-implemented",
+            responseTime:"not-implemented"
+        }
+    });
 })
